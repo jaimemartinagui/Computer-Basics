6fc712ff23c46f165b8ad568fb80dc81a211bb91
Jaime Martin Agui <jaimemartinagui@gmail.com>
2021-09-09 16:08:40 +0200

    convert to negative

--- binary_subtraction.py
+++ binary_subtraction.py
@@ -5,13 +5,25 @@
 from basic_logic_gates import not_
 from binary_addition import general_adder
 
-def convert_to_negative(positive_binary_number):
-    """Function to convert a positive binary number to negative with the Two's complement approach."""
+def general_subtractor(a, b):
+    """
+    General binary subtractor.
+    Subtracts b from a.
+    """
 
-    if positive_binary_number[0] == '1':
-        positive_binary_number = '0' + positive_binary_number
+    pass
 
-    inverted_bits_list = [not_(int(bit)) for bit in positive_binary_number]
-    inverted_number    = ''.join(str(bit) for bit in inverted_bits_list)
+    # negative_b = convert_to_negative(b)
 
-    return general_adder(inverted_number, '1')
+    # return general_adder(a, negative_b)
+
+def convert_to_correct_format(binary_number, negative=False):
+    """Function to convert binary numbers to Two's complement format."""
+
+    formatted_number = '0' + binary_number
+
+    if negative:
+        inverted_bits_list = [not_(int(bit)) for bit in formatted_number]
+        formatted_number    = ''.join(str(bit) for bit in inverted_bits_list)
+
+    return formatted_number
